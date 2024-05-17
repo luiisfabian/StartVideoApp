4a3ce3932f08ee51b063213cff509986dcd79000
FabianM <luiisfabian96@gmail.com>
2024-05-17 11:42:41 -0500

    Inicializacion y organizaccion de la aplicacion

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:app_video_reproducer/config/AppTheme.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Start Video',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
